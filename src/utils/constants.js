c9a60dbce863b90f017f060c4100e62ec3591aa2
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-09 23:55:20 +0530

    constants file created with testimonials data

---
+++ src/utils/constants.js
@@ -0,0 +1,29 @@
+import test1 from "../assets/test1.png";
+import test2 from "../assets/test2.png";
+import test3 from "../assets/test3.png";
+
+const testimonials = [
+	{
+		img: test1,
+		name: "Marty Weiner",
+		desc: "Former CTO, Reddit Ideaflow Advisor",
+		content:
+			"“A key step to realizing the potential of the Web as a medium for collective intelligence.”",
+	},
+	{
+		img: test2,
+		name: "John Devadoss",
+		desc: "Founding Director, Microsoft Digital Consulting",
+		content:
+			"“Ideaflow is the force multiplier for business productivity in this new normal.”",
+	},
+	{
+		img: test3,
+		name: "Tim Draper",
+		desc: "Founder, DFJ Ideaflow Investor",
+		content:
+			"“The amount of value that can be unlocked by connecting the right people and ideas together is incredible. I’m very excited for what Ideaflow is building.”",
+	},
+];
+
+export default testimonials;
