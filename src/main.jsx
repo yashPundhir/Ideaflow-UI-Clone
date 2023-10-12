a878f956fdcdf91704e8f3ece6de22b54bfbd0f6
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-12 10:48:37 +0530

    routing mechanism implemneted

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Homepage from "./pages/Homepage";
+import "./index.css";
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
+import Career from "./pages/Career.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// const router = createBrowserRouter([
+// 	{
+// 		path: "/",
+// 		element: <App />,
+// 		children: [
+// 			{
+// 				path: "",
+// 				element: <Homepage />,
+// 			},
+// 			{
+// 				path: "career",
+// 				element: <Career />,
+// 			},
+// 		],
+// 	},
+// ]);
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route path="" element={<Homepage />} />
+			<Route path="career" element={<Career />} />
+		</Route>
+	)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		{/* <App /> */}
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
