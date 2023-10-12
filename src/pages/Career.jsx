109f3ac437f6bc88a99cfaab1e637270357fc67f
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-12 10:37:46 +0530

    Career page created

---
+++ src/pages/Career.jsx
@@ -0,0 +1,23 @@
+// import React from "react
+
+import Advisors from "../components/Advisors";
+import Future from "../components/Future";
+import Leadership from "../components/Leadership";
+import Mission from "../components/Mission";
+import Positions from "../components/Positions";
+import WhoWeAre from "../components/WhoWeAre";
+
+const Career = () => {
+	return (
+		<>
+			<Future />
+			<Mission />
+			<Advisors />
+			<Leadership />
+			<WhoWeAre />
+			<Positions />
+		</>
+	);
+};
+
+export default Career;
