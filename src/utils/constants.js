f68ff068dfdac715bf8841523066f6e0e8835acb
yashPundhir <yash.pundhir.prof@gmail.com>
2023-10-12 10:44:53 +0530

    constants utility file created

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,16 @@
 import test1 from "../assets/test1.png";
 import test2 from "../assets/test2.png";
 import test3 from "../assets/test3.png";
+import adv1 from "../assets/adv1.png";
+import adv2 from "../assets/adv2.png";
+import adv3 from "../assets/adv3.png";
+import adv4 from "../assets/adv4.png";
+import adv5 from "../assets/adv5.png";
+import adv6 from "../assets/adv6.png";
+import lead1 from "../assets/lead1.png";
+import lead2 from "../assets/lead2.png";
+import lead3 from "../assets/lead3.png";
+import lead4 from "../assets/lead4.png";
 
 const testimonials = [
 	{
@@ -23,6 +33,101 @@ const testimonials = [
 		desc: "Founder, DFJ Ideaflow Investor",
 		content:
 			"“The amount of value that can be unlocked by connecting the right people and ideas together is incredible. I’m very excited for what Ideaflow is building.”",
+	},
+];
+
+export const advisors = [
+	{
+		img: adv1,
+		name: "Naval Ravikant",
+		desc: "Founder of AngelList",
+	},
+	{
+		img: adv2,
+		name: "Joe Lonsdale",
+		desc: "Founder of Palantir",
+	},
+	{
+		img: adv3,
+		name: "Marty Weiner",
+		desc: "Former CTO of Reddit",
+	},
+	{
+		img: adv4,
+		name: "Tim Draper",
+		desc: "Founder of Draper Associates",
+	},
+	{
+		img: adv5,
+		name: "Kelvin Beachum Jr.",
+		desc: "Investor, Humanitarian, NFL Player",
+	},
+	{
+		img: adv6,
+		name: "Ryan Delk",
+		desc: "Founder of Primer",
+	},
+];
+
+export const leadership = [
+	{
+		img: lead1,
+		name: "Jacob Cole",
+		desc: "CEO",
+		content:
+			"Past: MIT Media Lab; researcher with Tim Berners-Lee. Venture Partner at Outliers Fund.",
+	},
+	{
+		img: lead2,
+		name: "David Greenspan",
+		desc: "Head of Product",
+		content:
+			"Past: Co-Founder of AppJet, created Etherpad (acquired by Google); YC W07. 1st Engineer at Meteor.",
+	},
+	{
+		img: lead3,
+		name: "Bat Manson",
+		desc: "Head of Engineering",
+		content:
+			"Past: Serial Entrepreneur. Engineering Manager at Apple. Director of Engineering at Shone; YC W18.",
+	},
+	{
+		img: lead4,
+		name: "Albert Sławiński",
+		desc: "Full Stack Engineer",
+		content: "Past: Software Engineer at Palantir. Oxford Alum.",
+	},
+];
+
+export const positions = [
+	// {
+	// 	title: "Full Stack Engineer",
+	// 	location: "Remote",
+	// 	desc: "Who has significant frontend experience and is up to the challenge of writing something as complex and high performance as an IDE in JavaScript.",
+	// },
+	{
+		title: "iOS Engineer",
+		location: "Remote",
+		desc: "Who wants to build the best-ever, low-friction personal notes app.",
+		link: "https://www.ideaflow.io/ios-engineer",
+	},
+	{
+		title: "Product Designer",
+		location: "Remote",
+		desc: "Who has an excellent product sense, as well as exceptional user experience & user interface skills.",
+		link: "https://www.ideaflow.io/product-designer",
+	},
+	{
+		title: "Marketing Associate",
+		location: "Remote",
+		desc: "Who is an outstanding writer and strategic mind.",
+		link: "https://www.ideaflow.io/marketing-associate",
+	},
+	{
+		title: "ML (NLP) Engineer",
+		location: "Remote",
+		desc: "Who has the gumption to write something on par with a new search engine. Ideally you are familiar with dependency parse trees and have written an interpreter in the past.",
+		link: "https://www.ideaflow.io/ml-engineer",
 	},
 ];
 
